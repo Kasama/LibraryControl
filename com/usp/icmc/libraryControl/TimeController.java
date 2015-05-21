6c91db190a572782e851f6d3f7b6d6e956074dc4
Kasama <ralegro@bol.com.br>
2015-05-20 21:22:20 -0300

    Implemented base of TimeController

--- com/usp/icmc/libraryControl/TimeController.java
+++ com/usp/icmc/libraryControl/TimeController.java
@@ -2,12 +2,17 @@ package com.usp.icmc.libraryControl;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class TimeController {
 
-    private static Calendar calendar;
+    private static Calendar calendar = new GregorianCalendar();
 
     private TimeController(){}
+
+    public static void setDate(Date date){
+        calendar.setTime(date);
+    }
 
     public static Date getDate(){
         return calendar.getTime();
