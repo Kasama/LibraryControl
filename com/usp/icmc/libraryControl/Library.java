f6194f017630b11589ead506c0924f287fd7ab31
Kasama <ralegro@bol.com.br>
2015-05-24 00:51:33 -0300

    removed old csv parser, adding new one

--- com/usp/icmc/libraryControl/Library.java
+++ com/usp/icmc/libraryControl/Library.java
@@ -1,6 +1,10 @@
 package com.usp.icmc.libraryControl;
 
+import com.opencsv.CSVReader;
+
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,31 +15,80 @@ public class Library {
     private ArrayList<Book> books;
     private Map<User, Integer> blacklist;
 
-    public Library(String dataFolder) throws IllegalArgumentException{
+    public Library(String dataDirectory) throws IllegalArgumentException {
 
         books = new ArrayList<>();
         blacklist = new HashMap<>();
 
-        File file = new File(dataFolder);
-        if(!file.isDirectory())
+        if (dataDirectory.endsWith("/"))
+            dataDirectory = dataDirectory
+                    .substring(0, dataDirectory.length() - 2);
+
+        File file = new File(dataDirectory);
+        File booksDirectory = new File(dataDirectory + "/books");
+        File usersDirectory = new File(dataDirectory + "/users");
+        File booksFile = new File(dataDirectory + "/books.csv");
+        File usersFile = new File(dataDirectory + "/users.csv");
+        File blacklistFile = new File(dataDirectory + "/blacklist.csv");
+        File[] allFiles;
+
+        if (file.exists() && !file.isDirectory())
             throw new IllegalArgumentException("Not a directory");
+
+        if(
+                !booksFile.exists() ||
+                !blacklistFile.exists() ||
+                !usersFile.exists()
+        ){
+            try {
+                booksFile.createNewFile();
+                blacklistFile.createNewFile();
+                booksDirectory.createNewFile();
+                usersDirectory.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                // TODO proper exception handling
+            }
+        }else{
+            try {
+                CSVReader csvReader = new CSVReader(new FileReader(booksFile));
+                String[] tokens;
+                while ((tokens = csvReader.readNext()) != null){
+                    books.add(
+                            new Book(
+                                    tokens[0],
+                                    tokens[1],
+                                    tokens[2].equals("true")
+                            )
+                    );
+                }
+                csvReader = new CSVReader(new FileReader(blacklistFile));
+                while ((tokens = csvReader.readNext()) != null){
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                // TODO proper exception handling
+            }
+        }
+
 
     }
 
-    public void addBook(Book book){
+    public void addBook(Book book) {
         books.add(book);
     }
 
-    public void rentBook(User user, Book book){
-        if(!user.canRentBook()) return;
-        if(blacklist.containsKey(user)) return;
+    public void rentBook(User user, Book book) {
+        if (!user.canRentBook()) return;
+        if (blacklist.containsKey(user)) return;
 
         user.rentBook(book);
         book.setAvailableForRental(false);
         book.writeRentalLog(user);
     }
 
-    public boolean doesBookExist(String Author, String Title){
+    public boolean doesBookExist(String Author, String Title) {
         Optional<Book> b;
         b = books.stream()
                 .filter(book -> book.getAuthor().equals(Author))
@@ -45,7 +98,7 @@ public class Library {
         return b.isPresent();
     }
 
-    public boolean doesBookExist(int id){
+    public boolean doesBookExist(int id) {
         Optional<Book> b;
         b = books.stream()
                 .filter(book -> book.getId() == id)
@@ -54,24 +107,24 @@ public class Library {
         return b.isPresent();
     }
 
-    public Book getBook(String Author, String Title){
+    public Book getBook(String Author, String Title) {
         Optional<Book> b;
         b = books.stream()
                 .filter(book -> book.getAuthor().equals(Author))
                 .filter(book -> book.getTitle().equals(Title))
                 .filter(Book::isAvailableForRental)
                 .findFirst();
-        if(!b.isPresent()) throw new noBookFoundException();
+        if (!b.isPresent()) throw new noBookFoundException();
         return b.get();
     }
 
-    public Book getBook(int id){
+    public Book getBook(int id) {
         Optional<Book> b;
         b = books.stream()
                 .filter(book -> book.getId() == id)
                 .filter(Book::isAvailableForRental)
                 .findFirst();
-        if(!b.isPresent()) throw new noBookFoundException();
+        if (!b.isPresent()) throw new noBookFoundException();
         return b.get();
     }
 
