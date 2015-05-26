f5672c09bb4edb506d57fa129f145c4c732ea75a
Kasama <ralegro@bol.com.br>
2015-05-26 20:52:42 -0300

    finished startScene

--- com/usp/icmc/libraryControl/Main.java
+++ com/usp/icmc/libraryControl/Main.java
@@ -1,12 +1,25 @@
 package com.usp.icmc.libraryControl;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+            getClass().getResource("startScene.fxml")
+        );
+        Parent root = loader.load();
+        primaryStage.setTitle("Library Controller");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setHeight(270);
+        primaryStage.setWidth(500);
+        primaryStage.show();
 
     }
 
---
+++ com/usp/icmc/libraryControl/startSceneController.java
@@ -0,0 +1,160 @@
+package com.usp.icmc.libraryControl;
+
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.stage.DirectoryChooser;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.*;
+
+public class startSceneController implements Initializable {
+
+    @FXML
+    Button add;
+    @FXML
+    Button remove;
+    @FXML
+    Button loadLibrary;
+    @FXML
+    ListView<String> libraryList;
+    @FXML
+    DatePicker datePicker;
+
+    @Override
+    public void initialize(
+        URL location, ResourceBundle resources
+    ) {
+        ObservableList<String> libraryListElements = FXCollections
+            .observableArrayList();
+        File file = new File("libraryList.csv");
+        if (file.exists()) {
+            try {
+                CSVReader csvReader = new CSVReader(new FileReader(file));
+                String[] tokens;
+                while ((tokens = csvReader.readNext()) != null)
+                    libraryListElements.add(tokens[0]);
+            } catch (IOException ignored) {
+                // TODO proper catching
+            }
+        }
+        libraryList.setItems(libraryListElements);
+        datePicker.setValue(LocalDate.now());
+    }
+
+    @FXML
+    private void addNewLibrary(Event e) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select a folder containing a library");
+        directoryChooser.setInitialDirectory(new File("."));
+        File selectedDirectory;
+        selectedDirectory = directoryChooser.showDialog(
+            (((Button) e.getSource()).getScene()).getWindow()
+        );
+        System.out.println(selectedDirectory.getPath());
+        try {
+            libraryList.getItems().add(selectedDirectory.getPath());
+        } catch (Exception e1) {
+            System.out.println("fodeu");
+        }
+        updateLibrariesFile();
+    }
+
+    private void updateLibrariesFile() {
+        File file = new File("libraryList.csv");
+        for (String s : libraryList.getItems()) {
+            String[] nextLine = new String[1];
+            nextLine[0] = s;
+            try {
+                if (!file.createNewFile()) {
+                    file.delete();
+                    file.createNewFile();
+                }
+                CSVWriter csvWriter = new CSVWriter(new FileWriter(file));
+                csvWriter.writeNext(nextLine);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    private void removeSelectedLibrary() {
+        libraryList.getItems()
+            .remove(libraryList.getSelectionModel().getSelectedItem());
+        updateLibrariesFile();
+    }
+
+    @FXML
+    private void loadLibrary() {
+        String path = null;
+        path = libraryList.getSelectionModel().getSelectedItem();
+        File file = null;
+        if (path == null)
+            return;
+        file = new File(path);
+
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                File[] filesArr = file.listFiles();
+                ArrayList<File> files;
+                if (filesArr != null) {
+                    files = new ArrayList<>(Arrays.asList(filesArr));
+                    long l = files
+                        .stream()
+                        .map(File::getAbsolutePath)
+                        .distinct()
+                        .parallel()
+                        .filter(
+                            f ->
+                                f.contains("book.csv") ||
+                                f.contains("users.csv") ||
+                                f.contains("blacklist.csv") ||
+                                f.contains("/book") ||
+                                f.contains("/users")
+                        )
+                        .count();
+                    if (l != 5) {
+                        Alert confirmCreateNewLibrary = new Alert(
+                            Alert.AlertType.CONFIRMATION,
+                            "The selected library does not exist, do you want" +
+                            " to create a new one?",
+                            ButtonType.NO,
+                            ButtonType.YES
+                        );
+                        confirmCreateNewLibrary.setTitle("Library exists");
+                        confirmCreateNewLibrary
+                            .setHeaderText("Library exists!");
+                        Optional<ButtonType> selection
+                            = confirmCreateNewLibrary.showAndWait();
+                        if (selection.isPresent() &&
+                            selection.get().equals(ButtonType.YES))
+                        {
+                            java.sql.Date d = java.sql.Date.valueOf(
+                                datePicker.getValue()
+                            );
+                            Date date = new Date(d.getTime());
+                            TimeController timeController = TimeController
+                                .getInstance();
+                            timeController.setDate(date);
+                            // TODO skip to next scene
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+}
